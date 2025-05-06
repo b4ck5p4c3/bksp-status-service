c4dcf246f6d51acc22ff8ad28c5940da1ed8a1a0
Egor Koleda <radioegor146@ctf.by>
2025-05-07 01:25:57 +0300

    main commit

---
+++ src/main.ts
@@ -0,0 +1,53 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = parseInt(process.env.PORT ?? "8010");
+const NODERED_PUSH_TOKEN = process.env.NODERED_PUSH_TOKEN ?? "nodered-push-token";
+
+const app = express();
+
+enum SpacePowerStatus {
+    UNKNOWN = "unknown",
+    ON = "on",
+    OFF = "off"
+}
+
+const currentStatus = {
+    spacePower: SpacePowerStatus.UNKNOWN
+};
+
+app.get("/status", (req, res) => {
+    res.json(currentStatus);
+});
+
+app.use("/push/", (req, res, next) => {
+    if (!req.headers.authorization) {
+        res.status(403).end();
+        return;
+    }
+    const parts = req.headers.authorization.split(" ");
+    if (parts.length != 2) {
+        res.status(403).end();
+        return;
+    }
+    if (parts[0] !== "Token" || parts[1] !== NODERED_PUSH_TOKEN) {
+        res.status(403).end();
+        return;
+    }
+    next();
+})
+
+app.post("/push/space-power", (req, res) => {
+    switch (req.body.status) {
+        case "on":
+            currentStatus.spacePower = SpacePowerStatus.ON;
+            break;
+        case "off":
+            currentStatus.spacePower = SpacePowerStatus.OFF;
+            break;
+    }
+})
+
+app.listen(PORT);
